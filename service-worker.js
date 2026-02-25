8e378c2a49e4042f513dacb43a21f880287c517e
rickterrac137 <ops.barry.allen@gmail.com>
2026-02-25 17:57:30 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "v1.2.0";
+const APP_VERSION = "v1.3.0";
 const CACHE_NAME = "estoque-rick-" + APP_VERSION;
 
 const FILES = [
@@ -39,3 +39,4 @@ self.addEventListener("fetch", event => {
       .catch(() => caches.match(event.request))
   );
 });
+
