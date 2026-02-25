e95628464b1047a59227bbb71f13b092f3e46bcd
rickterrac137 <ops.barry.allen@gmail.com>
2026-02-25 12:50:42 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,24 @@
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open("rick-cache").then(c=>c.addAll(["index.html"]))
+const CACHE_NAME = "estoque-cache-v1";
+const urlsToCache = [
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
